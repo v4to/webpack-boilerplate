4c6004f3e01bbd873554442adf66a4c9df3650aa
humble-timon <andrew.egorcev@gmail.com>
2020-01-04 01:17:09 +0200

    Added React support

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
+import { App } from './components/App.jsx';
 
-ReactDOM.render();
+ReactDOM.render(<App />, document.getElementById('root'));
+
